7ef196815dd25ed642c9807b38a1aee93794ac40
Supian M <supianidz@gmail.com>
2023-03-01 10:49:52 +0800

    Initial

---
+++ app/Filament/Resources/UserResource/Pages/ListUsers.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Filament\Resources\UserResource\Pages;
+
+use App\Filament\Resources\UserResource;
+use Closure;
+use Filament\Pages\Actions;
+use Filament\Resources\Pages\ListRecords;
+use Filament\Resources\Table;
+use Filament\Tables\Actions\Action;
+use Filament\Tables\Columns\TextColumn;
+
+class ListUsers extends ListRecords
+{
+    protected static string $resource = UserResource::class;
+
+    protected function table(Table $table) : Table
+    {
+        return $table->columns([
+            TextColumn::make('name')->searchable()->sortable(),
+            TextColumn::make('email')->searchable()->sortable(),
+            TextColumn::make('created_at')->searchable()->sortable(),
+        ]);
+    }
+
+    /**
+     * @return Closure|null
+     */
+    protected function getTableRecordActionUsing() : ?Closure
+    {
+        return fn() : Action => Actions\DeleteAction::make();
+    }
+
+    protected function getActions() : array
+    {
+        return [
+            Actions\CreateAction::make(),
+        ];
+    }
+}
